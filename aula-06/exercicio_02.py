660f6c93161ec4522a75e33b24fd3610f3311f6c
Alan Ramos de Souza <alanramosdesouza@gmail.com>
2026-07-08 20:54:05 -0300

    Adiciona controle de estoque com POO

--- aula-06/exercicio_02.py
+++ aula-06/exercicio_02.py
@@ -6,6 +6,22 @@ class Produto:
 
     def calcular_valor_total(self):
         return self.preco * self.quantidade
+    
+    def vender(self, quantidade_vendida):
+        if quantidade_vendida <= 0:
+            print('Quantidade inválida.')
+        elif quantidade_vendida <= self.quantidade:
+            self.quantidade -= quantidade_vendida
+            print('Venda realizada.')
+        else:
+            print('Estoque insuficiente.')
+
+    def repor(self, quantidade_reposta):
+        if quantidade_reposta <= 0:
+            print('Quantidade inválida.')
+        else:
+            self.quantidade += quantidade_reposta
+            print('Estoque atualizado.')
 
 livro = Produto("Livro", 50.00, 3)
 caneta = Produto("Caneta", 2.50, 10)
@@ -14,6 +30,16 @@ total_caneta = caneta.calcular_valor_total()
 total_livro = livro.calcular_valor_total()
 
 print(f'Produto: {caneta.nome}')
-print(f'Total em estoque: {total_caneta:.2f}')
+print(f'Total em estoque: R$ {total_caneta:.2f}')
 print(f'Produto: {livro.nome}')
-print(f'Total em estoque: {total_livro:.2f}')
+print(f'Total em estoque: R$ {total_livro:.2f}')
+
+livro.vender(2)
+
+print(f"Quantidade restante: {livro.quantidade}")
+
+livro.repor(5)
+print(f"Quantidade atualizada: {livro.quantidade}")
+
+livro.repor(-3)
+print(f"Quantidade após tentativa inválida: {livro.quantidade}")
